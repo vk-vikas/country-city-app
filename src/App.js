91574240f3f7113921b71d55895241e13939e86a
vikas kumar <vk.vikas3003@gmail.com>
2023-01-14 16:20:30 +0530

    starting files setup done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState ,useEffect} from "react";
+import "./App.css";
+
 
 function App() {
+
+  const [county,setCounty] = useState([]);
+
+  useEffect(() => {
+    try {
+      
+    } catch (error) {
+      console.log(error);
+    }
+  }, [])
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>select your city</h1>
+      <div>
+        <select>
+          <option>India</option>
+        </select>
+        <select>
+          <option>Patna</option>
+        </select>
+        <button>submit</button>
+      </div>
     </div>
   );
 }
