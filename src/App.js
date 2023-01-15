8cf37b8beac1630984efbce0ba6f7a3355af4d0e
vikas kumar <vk.vikas3003@gmail.com>
2023-01-15 12:35:43 +0530

    all features added

--- src/App.js
+++ src/App.js
@@ -1,31 +1,87 @@
-import { useState ,useEffect} from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
-
+import axios from "axios";
 
 function App() {
+  const [countries, setCountries] = useState([]);
+  const [singleCountry, setsingleCountry] = useState("");
+  const [cities, setcities] = useState([]);
+  const [city, setcity] = useState("");
+  const [submit, setsubmit] = useState(false);
 
-  const [county,setCounty] = useState([]);
-
-  useEffect(() => {
+  const fetchCountries = async () => {
     try {
-      
+      const returnedData = await axios.get(
+        "https://countriesnow.space/api/v0.1/countries"
+      );
+
+      setCountries(returnedData.data.data);
     } catch (error) {
       console.log(error);
     }
-  }, [])
-  
+  };
+
+  const fetchCities = (passedCountry) => {
+    // so that if we change the country the display message goes away
+    setsubmit(false);
+    setcity(null);
+    setsingleCountry(passedCountry);
+    const findCities = countries.find((i) => i.country === passedCountry);
+    setcities(findCities.cities);
+  };
+
+  useEffect(() => {
+    fetchCountries();
+  }, []);
+
+  const submitHandler = (e) => {
+    {
+      singleCountry && city && setsubmit(true);
+    }
+  };
+
   return (
     <div className="App">
       <h1>select your city</h1>
       <div>
-        <select>
-          <option>India</option>
-        </select>
-        <select>
-          <option>Patna</option>
-        </select>
-        <button>submit</button>
+        {countries && (
+          <select
+            onChange={(e) => fetchCities(e.target.value)}
+            value={singleCountry}
+          >
+            <option disabled selected hidden>
+              select country
+            </option>
+            {countries.map((i) => (
+              // country is used to destructure the data that api gave
+              <option
+                key={`${i.country}-${Math.random() * 100}`}
+                value={i.country}
+              >
+                {i.country}
+              </option>
+            ))}
+          </select>
+        )}
+        {cities && (
+          <select onChange={(e) => setcity(e.target.value)} value={city}>
+            <option disabled hidden selected>
+              select city
+            </option>
+            {cities.map((i) => (
+              <option key={`${i}-${Math.random() * 1000}`}>{i}</option>
+            ))}
+          </select>
+        )}
+        <button onClick={submitHandler}>submit</button>
       </div>
+      {submit && (
+        <div>
+          <h2>
+            The country is {singleCountry} and the city is {city}
+          </h2>
+        </div>
+      )}
     </div>
   );
 }
